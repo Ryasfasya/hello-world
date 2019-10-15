9b3ac0dd693e1baebb99471d973a86dd16995a0b
M Ryas Fasya Ramadhan <fasyainside33@gmail.com>
2019-10-15 13:19:50 +0700

    Commit Ketiga.

--- app/src/main/java/com/apps/latihan1/MainActivity.java
+++ app/src/main/java/com/apps/latihan1/MainActivity.java
@@ -3,12 +3,28 @@ package com.apps.latihan1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+        private int skora = 0;
+        private int skorb = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambah3PointA(View view) {
+        skora = skora +3;
+        TextView textViewA = findViewById(R.id.skora);
+        textViewA.setText(String.valueOf(skora));
+    }
+
+    public void tambah3PointB(View view) {
+        skorb = skorb +3;
+        TextView textViewB = findViewById(R.id.skorb);
+        textViewB.setText(String.valueOf(skorb));
     }
 }
